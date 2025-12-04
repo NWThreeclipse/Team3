5a8a39d69c628b18a74ebddc6dd8696685441fd9
Dylan Patrascu <dylanpatrascu1@gmail.com>
2025-12-04 18:55:48 -0500

    Bug fixes

--- Assets/Scripts/Items/ItemDespawn.cs
+++ Assets/Scripts/Items/ItemDespawn.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 
 public class ItemDespawn : MonoBehaviour
 {
+    [SerializeField] private BarkManager barkManager;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Item"))
         {
+            Item item = collision.gameObject.GetComponent<Item>();
+            if (item.GetItemData().Rarity == Rarity.Anomalous)
+            {
+                barkManager.HidePlayerBark();
+            }
             Destroy(collision.gameObject);
+
 
         }
 
